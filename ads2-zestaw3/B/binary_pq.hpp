9b04d68ad822b06f4c9523fe8998386ddeccaa83
Eg-Nickname <djkubax@op.pl>
2026-04-09 17:05:04 +0200

    [s2] zagadnienia kolokwium i kod priority queue

---
+++ ads2-zestaw3/B/binary_pq.hpp
@@ -0,0 +1,84 @@
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+#ifndef BINARY_PQ_HPP
+
+template <typename T, typename CMP>
+class BinaryPq {
+    std::vector<T> heap{};
+    CMP cmp_fn;
+
+    public:
+    BinaryPq(CMP fn) : cmp_fn(fn) {}
+    void insert(T e);
+    T pop();
+
+    void display() {
+        for (auto& e : heap) {
+            std::cout << e << ",";
+        }
+    }
+
+    bool empty() { return this->heap.empty(); }
+};
+
+template <typename T, typename CMP>
+void BinaryPq<T, CMP>::insert(T e) {
+    heap.push_back(e);
+
+    size_t pos = heap.size() - 1;
+    while (true) {
+        if (pos == 0) {
+            break;
+        }
+
+        size_t parent = (pos - 1) / 2;
+
+        if (cmp_fn(heap[parent], heap[pos])) {
+            std::swap(heap[parent], heap[pos]);
+        } else {
+            break;
+        }
+        pos = parent;
+    }
+}
+
+template <typename T, typename CMP>
+T BinaryPq<T, CMP>::pop() {
+    if (heap.size() == 0) {
+        throw std::out_of_range("Queue empty");
+    }
+
+    std::swap(this->heap[0], this->heap.back());
+    T ret = heap.back();
+    heap.pop_back();
+
+    size_t pos = 0;
+    while (true) {
+        size_t lhs = pos * 2 + 1;
+        size_t rhs = pos * 2 + 2;
+
+        if (rhs >= heap.size()) {
+            if (lhs < heap.size() && !cmp_fn(heap[lhs], heap[pos])) {
+                std::swap(heap[lhs], heap[pos]);
+            }
+            break;
+        }
+        // We are at the end
+        // Get smaller priority child
+        if (!cmp_fn(heap[lhs], heap[rhs])) {
+            std::swap(heap[lhs], heap[pos]);
+            pos = lhs;
+        } else {
+            std::swap(heap[rhs], heap[pos]);
+            pos = rhs;
+        }
+    }
+
+    return ret;
+}
+
+#endif
---
+++ zagadnienia.md
@@ -0,0 +1,13 @@
+# Kolokwium 1 Zagadnienia
+
+- algorytmy zwiazane z kolejakami i zbiorami
+- listy łazon mogą sie pojawić
+- hashowanie w śmiesznej postaci (mod N)
+- hashowanie właściwe. Trzeba poczytac i się przygotować
+- dla kolejek zadania z kopcem binarnym
+- kilka linijek pseudo kodu / python / cpp
+- pytania o złożonośc obliczeniowa procedur, nie tylko O(N) lae też teta itd
+- zadanie żeby zmodyfikować kawałek kodu, żeby zmierzyć czas wykonania algorytmu (start, stop gdzie wstawić w kodzie)
+- dla list łaczonych zmaiast pseudokodu narysowac kilka obrazków (UwU) jak przełączyć pewnie. Lista łączona w pewnym stadium i mamy dodać element i narysować jak to się kurde balans dzieje.
+
+Kolokwium na połowe zajęć. Chyba, że będzie potrzeba to się kurde ciut przedłuży na całość.
